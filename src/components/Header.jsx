f960f267e0f47d90d0a376f300672186af42fd36
Ryan Gonzales <helloryantg@gmail.com>
2021-09-30 20:39:19 -0700

    convert header component to material ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
-import './Header.css'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  header: {
+    height: '100px',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }  
+})
 
 function Header() {
+  const classes = useStyles()
+
   return (
-    <div className="Header">
+    <div className={classes.header}>
       Photo Upload
     </div>
   )
