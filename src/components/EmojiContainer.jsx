97ec470c75eefdac21f7f037b50cb04d5296b7f4
Ryan Gonzales <helloryantg@gmail.com>
2021-09-30 20:52:40 -0700

    add unicorn emoji and ability to resize

--- src/components/EmojiContainer.jsx
+++ src/components/EmojiContainer.jsx
@@ -1,29 +1,45 @@
+import { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import Box from '@mui/material/Box'
 import Slider from '@mui/material/Slider'
-import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
   emojiContainer: {
     height: '200px',
     width: '100%',
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  unicornContainer: {
+    height: '100px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  unicorn: {
+    fontSize: ({ sliderValue }) => `${sliderValue}px`,
   }
 })
 
 function EmojiContainer() {
-  const classes = useStyles()
+  const [sliderValue, setSliderValue] = useState(50)
+
+  const classes = useStyles({ sliderValue })
 
   const onSliderChange = (event, value) => {
-    console.log('value', value)
+    setSliderValue(value)
   }
 
   return (
     <div className={classes.emojiContainer}>
-       <Box width={300}>
+      <Box width={300}>
         <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" onChange={onSliderChange} />
       </Box>
+      <div className={classes.unicornContainer}>
+        <div className={classes.unicorn}>🦄</div>
+      </div>
     </div>
   )
 }
