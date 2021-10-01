33fefdfe5062afa0737735554a4e2441c94754af
Ryan Gonzales <helloryantg@gmail.com>
2021-09-30 20:41:16 -0700

    convert PhotoContainer to material ui

--- src/components/PhotoContainer.jsx
+++ src/components/PhotoContainer.jsx
@@ -1,8 +1,21 @@
-import './PhotoContainer.css'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  photoContainer: {
+    height: '50%',
+    width: '100%',
+    backgroundColor: 'aliceblue',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 function PhotoContainer() {
+  const classes = useStyles()
+
   return (
-    <div className="PhotoContainer">
+    <div className={classes.photoContainer}>
       Photo Container
     </div>
   )
