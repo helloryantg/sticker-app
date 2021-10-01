83e265fd3b4d04e81c87536c11fd5d792d7e8b34
Ryan Gonzales <helloryantg@gmail.com>
2021-09-30 20:42:34 -0700

    update App to material ui

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { makeStyles } from '@material-ui/core/styles'
 import Header from '../components/Header'
 import PhotoContainer from '../components/PhotoContainer'
 import EmojiContainer from '../components/EmojiContainer'
 
+const useStyles = makeStyles({
+  app: {
+    height: '100%',
+  }
+})
+
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App">
+    <div className={classes.app}>
       <Header />
       <PhotoContainer />
       <EmojiContainer />
